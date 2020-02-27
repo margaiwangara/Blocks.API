461f3a199b3fb8d7546fb900a732a02bb12646a7
margaiwangara <margaiwangara@gmail.com>
2020-02-27 16:33:40 +0300

    modified db_uri to include testing db, modified testing script to set env to testing

--- models/index.js
+++ models/index.js
@@ -8,9 +8,14 @@ const mongoConfig = {
   useUnifiedTopology: true,
 };
 
+// db based on env
+const { NODE_ENV, MONGO_URI, MONGO_URI_TESTING } = process.env;
+
+const DB_URI = NODE_ENV == 'testing' ? MONGO_URI_TESTING : MONGO_URI;
+
 async function ConnectDB() {
   try {
-    const connect = await mongoose.connect(process.env.MONGO_URI, mongoConfig);
+    const connect = await mongoose.connect(DB_URI, mongoConfig);
     console.log(`MongoDB Connected: ${connect.connection.host}`);
   } catch (error) {
     console.log(error);
