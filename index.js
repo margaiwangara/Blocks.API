80a957f206cd2e2d11165d878772d8946f452d9f
margaiwangara <margaiwangara@gmail.com>
2020-02-27 14:59:18 +0300

    created error handling middleware files and class handler

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const dotenv = require('dotenv');
 const path = require('path');
 const connectDB = require('./models');
+const errorMiddleware = require('./middleware/error');
+const ExceptionHandler = require('./handlers/ExceptionHandler');
 
 // init
 const app = express();
@@ -14,6 +16,14 @@ app.use(express.json());
 
 // connect db
 connectDB();
+
+// error handling
+app.use(function(req, res, next) {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
+app.use(errorMiddleware);
 
 // PORT
 const PORT = parseInt(process.env.PORT || 5000, 10);
