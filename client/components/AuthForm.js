31e14206b2fd91b0f710f39f82cd40837b00d60b
margaiwangara <margaiwangara@gmail.com>
2020-02-28 20:04:59 +0300

    initial styling of login and register form and entire site layout

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import styled from 'styled-components';
 import { AuthContext } from '../context/appContext';
 import { authFormHook } from '../hooks/forms';
 
@@ -10,47 +11,64 @@ const AuthForm = ({ path: formType, btnText }) => {
     },
   } = useContext(AuthContext);
   return (
-    <form method="POST" autoComplete="off" onSubmit={handleSubmit}>
-      {formType == 'register' && (
-        <>
-          <label htmlFor="nameField">Name</label>
-          <input
-            type="text"
-            name="name"
-            id="nameField"
-            onChange={handleChange}
-            value={value.name}
-          />
-          <label htmlFor="nameField">Surname</label>
-          <input
-            type="text"
-            name="surname"
-            id="surnameField"
-            onChange={handleChange}
-            value={value.surname}
-          />
-        </>
-      )}
-      <label htmlFor="emailField">Email</label>
-      <input
-        type="email"
-        name="email"
-        id="emailField"
-        onChange={handleChange}
-        value={value.email}
-      />
-      <label htmlFor="passwordField">Password</label>
-      <input
-        type="password"
-        name="password"
-        id="passwordField"
-        onChange={handleChange}
-        value={value.password}
-      />
-      <button type="submit">{btnText}</button>
-      {!!Object.keys(error).length && <p>{error.message}</p>}
-    </form>
+    <FormContainer>
+      <Title>{btnText}</Title>
+      <form method="POST" autoComplete="off" onSubmit={handleSubmit}>
+        {formType == 'register' && (
+          <>
+            <label htmlFor="nameField">Name</label>
+            <input
+              type="text"
+              name="name"
+              id="nameField"
+              onChange={handleChange}
+              value={value.name}
+            />
+            <label htmlFor="nameField">Surname</label>
+            <input
+              type="text"
+              name="surname"
+              id="surnameField"
+              onChange={handleChange}
+              value={value.surname}
+            />
+          </>
+        )}
+        <label htmlFor="emailField">Email</label>
+        <input
+          type="email"
+          name="email"
+          id="emailField"
+          onChange={handleChange}
+          value={value.email}
+        />
+        <label htmlFor="passwordField">Password</label>
+        <input
+          type="password"
+          name="password"
+          id="passwordField"
+          onChange={handleChange}
+          value={value.password}
+        />
+        <button type="submit">{btnText}</button>
+        {!!Object.keys(error).length && (
+          <p className="error">{error.message}</p>
+        )}
+      </form>
+    </FormContainer>
   );
 };
 
+const FormContainer = styled.div`
+  width: 50%;
+  padding: 10px;
+  box-sizing: border-box;
+  margin: 0 auto;
+`;
+
+const Title = styled.h1`
+  letter-spacing: 0.75px;
+  text-align: center;
+  color: var(--accent);
+`;
 export default AuthForm;
--- client/hocs/withLayout.js
+++ client/hocs/withLayout.js
@@ -1,5 +1,5 @@
 import { AuthProvider } from '../context/appContext';
-import { createGlobalStyle } from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 import Header from '../containers/Header';
 
 function withLayout(Page) {
@@ -7,7 +7,9 @@ function withLayout(Page) {
     <AuthProvider>
       <GlobalStyle />
       <Header />
-      <Page />
+      <Container>
+        <Page />
+      </Container>
     </AuthProvider>
   );
 }
@@ -30,6 +32,67 @@ a {
   cursor: pointer;
 }
 
+.error {
+  width: 100%;
+  padding: 15px;
+  text-align: left;
+  color: #000;
+  background-color: rgba(255, 0, 0, 0.25);
+  border-left: solid 3px rgba(255, 0, 0, 0.7);
+  border-radius: 10px;
+  font-weight: 500;
+  letter-spacing: 0.75px;
+}
+
+input[type='text'], input[type='email'], input[type='password'] {
+  padding: 10px;
+  font-size: 0.9rem;
+  border-radius: 10px;
+  border: solid 1px var(--accent);
+
+  &:focus {
+    outline: none;
+  }
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  line-height: 1.5;
+  font-size: 0.9rem;
+  input {
+    margin-bottom: 10px;
+  }
+
+  button {
+    background: var(--accent);
+    border: solid 2px transparent;
+    padding: 10px 5px;
+    text-align: center;
+    text-transform: uppercase;
+    border-radius: 10px;
+    color: var(--primary);
+    cursor: pointer;
+    font-size: 0.9rem;
+    font-weight: 600;
+    outline: none;
+
+    &:hover {
+      background: var(--primary);
+      color: var(--accent);
+      border: solid 2px var(--accent);
+    }
+  }
+}
+`;
+
+const Container = styled.section`
+  width: 90%;
+  overflow-x: hidden;
+  box-sizing: border-box;
+  padding: 10px;
+  margin: 0 auto;
+  display: flex;
 `;
 
 export default withLayout;
