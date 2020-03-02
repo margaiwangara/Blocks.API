564e1fdc07c04add50652b7099861e68404c137b
margaiwangara <margaiwangara@gmail.com>
2020-03-02 09:31:43 +0300

    created realtors model, modified listing model with an association to realtors

--- models/listing.js
+++ models/listing.js
@@ -1,68 +1,85 @@
 const mongoose = require('mongoose');
 
-const listingSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, 'Please add a listing title'],
-    unique: true,
-    trim: true,
-    maxlength: [100, 'Listing title cannot be more than 100 characters'],
-  },
-  slug: String,
-  description: {
-    type: String,
-    required: [true, 'Please add a listing description'],
-    maxlength: [500, 'Listing description cannot be more than 500 character'],
-  },
-  address: {
-    type: String,
-    required: [true, 'Please add a listing address'],
-  },
-  location: {
-    // GeoPoint
-    type: {
+const listingSchema = new mongoose.Schema(
+  {
+    title: {
       type: String,
-      enum: ['Point'],
+      required: [true, 'Please add a listing title'],
+      unique: true,
+      trim: true,
+      maxlength: [100, 'Listing title cannot be more than 100 characters'],
     },
-    coordinates: {
-      type: [Number],
-      index: '2dsphere',
+    slug: String,
+    description: {
+      type: String,
+      trim: true,
+      required: [true, 'Please add a listing description'],
+      maxlength: [
+        500,
+        'Listing description cannot be more than 500 characters',
+      ],
     },
-    formattedAddress: String,
-    city: String,
-    state: String,
-    zipcode: String,
-    country: String,
-  },
-  price: {
-    type: Number,
-    required: [true, 'Please add a listing price'],
-  },
-  features: {
-    bedrooms: Number,
-    bathrooms: Number,
-    garage: {
+    address: {
+      type: String,
+      required: [true, 'Please add a listing address'],
+    },
+    location: {
+      // GeoPoint
+      type: {
+        type: String,
+        enum: ['Point'],
+      },
+      coordinates: {
+        type: [Number],
+        index: '2dsphere',
+      },
+      formattedAddress: String,
+      city: String,
+      state: String,
+      zipcode: String,
+      country: String,
+    },
+    price: {
       type: Number,
-      default: 0,
+      required: [true, 'Please add a listing price'],
     },
-    sqft: Number,
-    lotSize: Number,
-  },
-  listDate: {
-    type: Date,
-    default: Date.now,
-  },
-  photoMain: {
-    type: String,
-    default: 'no-image.jpg',
-  },
-  photos: [
-    {
+    features: {
+      bedrooms: Number,
+      bathrooms: Number,
+      garage: {
+        type: Number,
+        default: 0,
+      },
+      sqft: Number,
+      lotSize: Number,
+    },
+    listDate: {
+      type: Date,
+      required: [true, 'Please enter a listing date'],
+    },
+    photoMain: {
+      type: String,
+      default: 'no-image.jpg',
+    },
+    photos: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Photo',
+      },
+    ],
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    realtor: {
       type: mongoose.Schema.ObjectId,
-      ref: 'Photo',
+      ref: 'Realtor',
     },
-  ],
-});
+  },
+  {
+    timestamps: true,
+  },
+);
 
 const Listing = mongoose.model('Listing', listingSchema, 'listings');
 
